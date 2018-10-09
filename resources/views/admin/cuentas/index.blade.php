a47cb84c9e9d1bbb06dec42f0af7e5b13b69a811
Jonathan Gomez <jonakmex@gmail.com>
2018-10-09 18:09:58 -0500

    Style for tables on index pages for mobile seeing

--- resources/views/admin/cuentas/index.blade.php
+++ resources/views/admin/cuentas/index.blade.php
@@ -42,7 +42,7 @@
     <!-- Main content -->
     <section class="content">
         <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
               <h3 class="box-title">Cuentas</h3>
--- resources/views/admin/vivienda/index.blade.php
+++ resources/views/admin/vivienda/index.blade.php
@@ -46,7 +46,7 @@
         <div class="row">
         <div class="col-xs-12">
           <div class="box">
-            
+
             <!-- /.box-header -->
             <div class="box-body">
 
