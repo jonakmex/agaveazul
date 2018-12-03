3a0de2ed43eeb1f1c4add7d1d358995ce30a2858
Jonathan Gomez <jonakmex@gmail.com>
2018-12-03 08:11:32 -0600

    Logging token service

--- app/Service/ContactoService.php
+++ app/Service/ContactoService.php
@@ -5,22 +5,27 @@ use App\AvisoMail;
 use App\RegisterToken;
 use App\Profile;
 use Mail;
+use Log;
 
 class ContactoService
 {
   public static function crearTokenRegistro(Residente $residente){
+    Log::debug('crearTokenRegistro para '.$residente->nombre.'...');
     $token = new RegisterToken();
     $token->token = ContactoService::generarToken();
     $token->profile_id = 2;
     $token->status = 1;
     $token->residente_id = $residente->id;
-
+    Log::debug('Token generated.');
+    Log::debug('Retrieving profile '.$residente->id.'...');
     $profile = Profile::where(['descripcion'=>'Residente'])->firstOrFail();
-
+    Log::debug('Profile'.$profile->descripcion);
     $profile->tokens()->save($token);
 
     $data = array('registerToken'=>$token);
+    Log::debug('Sending mail to '.$residente->email.'...');
     Mail::to($residente->email)->queue(AvisoMail::newTokenRegistro($data));
+    Log::debug('Mail sent');
   }
 
   private static function generarToken($length = 10){
