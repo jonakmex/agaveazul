9f43afca072a1617f11a8ce43ecbcfa92ebcc6d0
Jonathan Gomez <jonakmex@gmail.com>
2018-12-03 08:59:14 -0600

    User saving

--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','profile_id',
+        'name', 'email', 'password','profile_id','residente_id',
     ];
 
     /**
