830527c7e32746e8522b53306f1b7eebfc2759a9
Jonathan Gomez <jonakmex@gmail.com>
2018-12-19 20:31:21 -0700

    Solucion de mail

--- config/mail.php
+++ config/mail.php
@@ -99,7 +99,7 @@ return [
     |
     */
 
-    'sendmail' => '/usr/sbin/sendmail -bs',
+    'sendmail' => env('MAIL_SENDMAIL', '/usr/sbin/sendmail -bs'),
 
     /*
     |--------------------------------------------------------------------------
@@ -117,6 +117,15 @@ return [
 
         'paths' => [
             resource_path('views/vendor/mail'),
+        ],
+    ],
+
+
+'stream' => [
+        'ssl' => [
+            'allow_self_signed' => true,
+            'verify_peer' => false,
+            'verify_peer_name' => false,
         ],
     ],
 
