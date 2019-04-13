f463154b5da4075fd132d9b5d16f7944dc084ecf
Jonathan Gomez <jonakmex@gmail.com>
2019-04-13 16:43:45 -0400

    Eliminamos la opción de acciones de la tabla Cuotas para el perfil Residente

--- resources/views/profiles/residente/cuotas/show.blade.php
+++ resources/views/profiles/residente/cuotas/show.blade.php
@@ -46,7 +46,6 @@
                  <thead>
                    <tr>
                      <th>Nombre</th>
-                     <th>Acciones</th>
                    </tr>
                  </thead>
                  <tbody>
