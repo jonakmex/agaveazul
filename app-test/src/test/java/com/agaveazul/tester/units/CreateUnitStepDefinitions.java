16bfc6e6ac6d812b4e82cc5abd8ecfef20ac7dc5
Jonathan Bustamante <jonakmex@gmail.com>
2021-03-26 09:18:10 -0400

    CC-33 Crating the failing tests

---
+++ app-test/src/test/java/com/agaveazul/tester/units/CreateUnitStepDefinitions.java
@@ -0,0 +1,49 @@
+package com.agaveazul.tester.units;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CreateUnitStepDefinitions {
+    @Given("I want to Register a Unit")
+    public void i_want_to_register_a_unit() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I provide the {string} and {string}")
+    public void i_provide_the_and(String description, String reference) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I confirm")
+    public void i_confirm() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see success message")
+    public void i_should_see_success_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I provide the  and {int}")
+    public void i_provide_the_and(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("I should see fail message")
+    public void i_should_see_fail_message() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("I provide the Casa {int} and ")
+    public void i_provide_the_casa_and(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
