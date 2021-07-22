bbe6740567beb1b26f75a788dce9caa3f1b57927
Jonathan Bustamante <jonakmex@gmail.com>
2021-07-21 20:24:08 -0400

    validate input port test PASSED

--- tests/Unit/CreateUnitUseCaseTest.php
+++ tests/Unit/CreateUnitUseCaseTest.php
@@ -7,13 +7,14 @@ use PHPUnit\Framework\TestCase;
 
 class CreateUnitUseCaseTest extends TestCase
 {
-    public function create_input_port()
+
+    public function test_create_input_port()
     {
         $inputPort = InputPortFactory::make("CreateUnitInputPort",["description"=>"Hello World"]);
         $this->assertEquals("Hello World",$inputPort->description);
     }
 
-    public function validate_input_port() 
+    public function test_validate_input_port() 
     {
         $inputPort = InputPortFactory::make("CreateUnitInputPort",["description"=>""]);
         $errors = $inputPort->validate();
