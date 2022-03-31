31d2f14d92e007b7a513ccf00bd37749e2801fb9
Jonathan Bustamante <jonakmex@gmail.com>
2022-03-31 00:33:11 -0400

    Installing Jetstream and AdminLTE

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,12 @@ Route::get('/', function () {
 });
 
 Route::resource('unit',App\Http\Controllers\UnitWebController::class);
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
