e65a87473d7816dd785afa84ea65571af959abc0
Carlos Sosa <carlos.alberto.sosa.perera@gmail.com>
2022-04-13 14:33:07 -0500

    including the modal component in the unit table

--- app/Http/Livewire/UnitTableComponent.php
+++ app/Http/Livewire/UnitTableComponent.php
@@ -2,38 +2,42 @@
 
 namespace App\Http\Livewire;
 
+use Livewire\Component;
 use App\Domains\Shared\Boundary\RequestFactory;
 use App\Domains\Shared\UseCase\UseCaseFactory;
 use App\Http\Controllers\ViewModel\UnitVm;
-use Livewire\Component;
+
+// define("FIND_UNITS_BY_CRITERIA_USE_CASE","App\Domains\Condo\UseCase\FindUnitsByCriteriaUseCase");
+// define("FIND_UNITS_BY_CRITERIA_REQUEST","App\Domains\Condo\Boundary\Input\FindUnitsByCriteriaRequest");
+// define("DELETE_UNIT_USE_CASE","App\Domains\Condo\UseCase\DeleteUnitUseCase");
+// define("DELETE_UNIT_REQUEST","App\Domains\Condo\Boundary\Input\DeleteUnitRequest");
 
 class UnitTableComponent extends Component
 {
+    const FIND_UNITS_BY_CRITERIA_USE_CASE = "App\Domains\Condo\UseCase\FindUnitsByCriteriaUseCase";
+    const DELETE_UNIT_USE_CASE = "App\Domains\Condo\UseCase\DeleteUnitUseCase";
+    const DELETE_UNIT_REQUEST = "App\Domains\Condo\Boundary\Input\DeleteUnitRequest";
+    const FIND_UNITS_BY_CRITERIA_REQUEST = "App\Domains\Condo\Boundary\Input\FindUnitsByCriteriaRequest";
+
     public $config;
     public $data;
     public $heads;
     public $description;
-    protected $listeners = ['refresh' => 'render'];
+    // protected $listeners = ['refresh' => 'render'];
     protected $queryString = ['description' => ['except' => '']];
 
-    public function mount(){
-        $this->heads = [
-            'ID',
-            'Description',
-            ['label' => 'Actions', 'no-export' => true, 'width'=> '20']
-        ];
+    private $requestFactory;
+    private $useCaseFactory;
+    private $findUnitsByCriteriaUseCase;
+    private $deleteUnitUseCase;
+    protected $listeners = ['deleteUnit'=>'deleteUnit'];
 
-        $this->config = [
-            'ordering' => false,
-            'paging'=> false,
-            'lengthChange' => false,
-            'info'=> false,
-            'searching' =>false
-        ];
-    }
-
-    public function render()
+    public function __construct()
     {
+        $this->requestFactory = app(RequestFactory::class);
+        $this->useCaseFactory = app(UseCaseFactory::class);
+        $this->findUnitsByCriteriaUseCase = $this->useCaseFactory->make(self::FIND_UNITS_BY_CRITERIA_USE_CASE);
+        $this->deleteUnitUseCase = $this->useCaseFactory->make(self::DELETE_UNIT_USE_CASE);
         $this->heads = [
             'ID',
             'Description',
@@ -47,20 +51,29 @@ class UnitTableComponent extends Component
             'info'=> false,
             'searching' => false,
         ];
+    }
 
-        $useCaseFactory = app(UseCaseFactory::class);
-        $requestFactory = app(RequestFactory::class);
-
-        $useCase = $useCaseFactory->make('App\Domains\Condo\UseCase\FindUnitsByCriteriaUseCase');
-
-        $request = $requestFactory->make(
-            'App\Domains\Condo\Boundary\Input\FindUnitsByCriteriaRequest',
-            [ 'description' => $this->description ]
-        );
-
-        $useCase->execute($request, function($response){
-            $this->data = UnitTableComponent::makeUnitVm($response->unitsDS);
+    public function mount()
+    {
+        $findUnitsByCriteriaRequest = $this->requestFactory->make(self::FIND_UNITS_BY_CRITERIA_REQUEST, ["description" => ""]);
+        $this->findUnitsByCriteriaUseCase->execute($findUnitsByCriteriaRequest, function($response){
+            if($response->errors) 
+                $this->data = [];
+            else
+                $this->data = UnitTableComponent::makeUnitVm($response->unitsDS);
         });
+    }
+
+    public function render()
+    {
+        $findUnitsByCriteriaRequest = $this->requestFactory->make(self::FIND_UNITS_BY_CRITERIA_REQUEST, ["description" => $this->description]);
+
+        $this->findUnitsByCriteriaUseCase->execute($findUnitsByCriteriaRequest, function($response){
+            if($response->errors) 
+                $this->data = [];
+            else
+                $this->data = UnitTableComponent::makeUnitVm($response->unitsDS);
+        });  
 
         return view('livewire.unit-table-component');
     }
@@ -71,9 +84,33 @@ class UnitTableComponent extends Component
             $unitVm = new UnitVm;
             $unitVm->id = $unitDS->id;
             $unitVm->description = $unitDS->description;
+            $actionEdit = '
+            <a href="'.route('unit.edit',$unitDS->id).'" class="btn btn-xs text-primary mx-1" title="Edit">
+                <i class="fa fa-lg fa-fw fa-pen"></i>
+            </a>';
+            $actionDelete = '
+            <button class="btn btn-xs text-danger mx-1" title="Delete"'."wire:click='confirmDeleteUnit($unitVm->id)'".' >
+                <i class="fa fa-lg fa-fw fa-trash"></i>
+            </button>';
+            $actionShow = '
+            <a href="'.route('unit.show',$unitDS->id).'" class="btn btn-xs text-teal mx-1" title="Show">
+                <i class="fa fa-lg fa-fw fa-eye"></i>
+            </a>';
+            $unitVm->buttons = $actionShow.$actionEdit.$actionDelete;
             array_push($unitsVm, $unitVm);
         }
         
         return $unitsVm;
+    }
+
+    public function confirmDeleteUnit($unitId){
+        $this->emit('unitSelected',$unitId);
+    }
+
+    public function deleteUnit($unitId){
+        $deleteUnitRequest = $this->requestFactory->make(self::DELETE_UNIT_REQUEST,['id'=>$unitId]);
+        $this->deleteUnitUseCase->execute($deleteUnitRequest,function($response){
+            $this->emit('actionCompleted');
+        });
     }
 }
--- resources/views/livewire/unit-table-component.blade.php
+++ resources/views/livewire/unit-table-component.blade.php
@@ -8,26 +8,11 @@
         <x-adminlte-datatable id="unit-table" class="elevation-1" :config="$config" :heads="$heads"
             head-theme="dark" striped bordered hoverable>
             @forelse($data as $row)
-                <tr>
-                    <td>{{ $row->id }}</td>
-                    <td>{{ $row->description }}</td>
-                    <td>
-                        <a href="{{ route('unit.show', $row->id) }}" class="btn btn-xs text-teal mx-1" title="Show">
-                            <i class="fa fa-lg fa-fw fa-eye"></i>
-                        </a>
-                        <a href="{{ route('unit.edit', $row->id) }}" class="btn btn-xs text-primary mx-1" title="Edit">
-                            <i class="fa fa-lg fa-fw fa-pen"></i>
-                        </a>
-                       <span>
-                         @livewire(
-                            'unit-modal-delete-component', 
-                            ['unitId'=>$row->id, 'unitDescription'=>$row->description], 
-                            key('confirm-delete'.$row->id.'-'.$loop->iteration)
-                         )
-                       </span>
-                    </td>
-                </tr>
-               
+               <tr>
+                    @foreach ($row as $cell)
+                        <td>{!! $cell !!}</td>
+                    @endforeach
+               </tr>
             @empty
                 <tr>
                     <td class="text-center" colspan="3">No data</td>
@@ -35,6 +20,6 @@
             @endforelse
         </x-adminlte-datatable>
     </div>
+     @livewire('confirmation-modal-component',['modalId'=>'deleteUnitConfirmationModal','action'=>'deleteUnit'])
     <br><br>
-
 </div>
